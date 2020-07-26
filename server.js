294a4cfbf3a1dabcfc276c7173adc7b001e168ac
Joabe Silva <joabesilva533@gmail.com>
2020-07-26 13:03:04 -0300

    add cors na aplicacao

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors')
 require('dotenv').config();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 mongoose.connect(process.env.URI_MONGO_DB, { useNewUrlParser: true, useUnifiedTopology: true });
 
